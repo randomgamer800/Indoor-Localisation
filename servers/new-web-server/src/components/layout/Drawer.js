c60d6392853dd55279e41ba0309f36be97fbecf7
randomgamer800 <73993052+randomgamer800@users.noreply.github.com>
2023-01-07 13:53:12 +0800

    Update Drawer.js

--- servers/new-web-server/src/components/layout/Drawer.js
+++ servers/new-web-server/src/components/layout/Drawer.js
@@ -99,7 +99,7 @@ class PersistentDrawerLeft extends Component {
                         <MenuIcon />
                         </IconButton>
                         <Typography component={Link} to='/' variant="h6" color="inherit">
-                            Device Serverlllllllllll
+                            Device Server
                         </Typography>
                         {/* <MapTools /> */}
                     </Toolbar>
